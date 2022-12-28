4328f3cff6bc5c5a60b35c6c7e5a7f9140109d91
Luca Vivona <lucavivona01@gmail.com>
2022-12-28 10:21:55 -0500

    Fixed: fromGenerator, forms, and query

--- ui/src/components/form/utils.js
+++ ui/src/components/form/utils.js
@@ -1,4 +1,106 @@
 import { v4 as uuidv4 } from "uuid";
+import { ParseError } from "./validate/validator";
+import {z} from 'zod';
+
+export const validateFormFieldInputs = (
+  uniqueIdsFormState,
+  globalFormState,
+  conditionalsFields,
+  context,
+  validationObject,
+  errordisplay,
+  setErrorDisplay
+) => {
+  const form = { ...uniqueIdsFormState, ...globalFormState };
+
+  let stopPopulatingProcess = false;
+  for (const key in validationObject) {
+    if (
+      conditionalsFields[key] &&
+      doesFieldNotMeetAllConditions(
+        conditionalsFields[key],
+        globalFormState,
+        context
+      )
+    ) {
+      continue;
+    }
+    let value = form[key]
+    console.log( )
+    if (z.optional()._def.typeName === validationObject[key]._def.typeName && value === ""){
+      value = undefined
+    }
+
+    const validate = validationObject[key].safeParse(value);
+    console.log(validate)
+
+    if (!validate.success) {
+      stopPopulatingProcess = true;
+      setErrorDisplay((err) => ({
+        ...err,
+        [key]: ParseError(validate.error.issues),
+      }));
+    } else if (errordisplay[key] !== null) {
+      setErrorDisplay((err) => ({ ...err, [key]: null }));
+    }
+  }
+  return stopPopulatingProcess;
+};
+
+export const connectionConsistencyCheck = (
+  doesFormExist,
+  relationalCardinalityToRoot,
+  relationalCardinalityToRefrenceKey,
+  constraints,
+  state
+) => {
+  
+  // const filterOutNonRequiredReferences = constraints.reference.filter((value) => {
+    
+  //   if ( value.override !== null && !value.override.required ) return false
+  //   return value.node.required
+  // }) 
+
+  // const sortedIds = sortSubmitterByFormId(state, filterOutNonRequiredReferences)
+  
+  // console.log(Boolean(doesFormExist), doesFormExist, constraints, relationalCardinalityToRoot, sortedIds)
+
+  
+  if (Boolean(doesFormExist)) return false; // the submitter does exist referenced to the root
+  console.log("Here");
+
+  if (
+    constraints.root !== null &&
+    relationalCardinalityToRoot >= constraints.root
+  )
+    return false; 
+    // if there is contraint relative root and 
+    // we meet that max constraints then return false
+    // as we can not construct anohter submitter under this form
+
+
+
+  // 1. from the root check if the required referenced submitteres exist
+  //if required submitter refereced does not exist
+  //  return false
+  //else
+  //  if relational cardinality to the root is not met or null
+  //  2. check the relational cardinality of the connection
+  //  if the cardinality is already met then
+  //    return false
+  //  else if the cardinality is not met
+  //    return true
+  return true
+};
+
+export const handleRelationshipCardinality = () => {
+
+}
+
+export const doesSumbitterExist = (data) => {
+  console.log(data)
+  return Boolean(data)
+}
 
 /**
  * 
@@ -56,19 +158,28 @@ export const getKeysValuePair = (keys, object) => {
   return tempObject
 }
 
-export const sortForeignKeys = (feildState, fks) => {
+export const sortSubmitterByFormId = (feildState, fks) => {
 
   let bucket = {} // initialize bucket will hold all the 
                   // primary keys of each foreign keys referenced in form
   
   // loop through all the reference_key (foreign keys)
   // dump them within there own form bucked
-  fks.forEach(fk => {
-    if (bucket[fk.node.primary_key_to.form_id] === undefined) {
-      bucket[fk.node.primary_key_to.form_id] = { "form" : fk.node.primary_key_to.form_id, "formPrimaryIdentifierKeys" : {}}
+  fks.forEach((fk) => {
+    if (bucket[fk.node.primaryFormIdentifiers.form_id] === undefined) {
+      bucket[fk.node.primaryFormIdentifiers.form_id] = {
+        form: fk.node.primaryFormIdentifiers.form_id,
+        formPrimaryIdentifierKeys: {},
+      };
     }
-    bucket[fk.node.primary_key_to.form_id] = {...bucket[fk.node.primary_key_to.form_id], "formPrimaryIdentifierKeys" : {...bucket[fk.node.primary_key_to.form_id].primaryKeys, ...getKeysValuePair([fk.node.name], feildState)}}    
-  })
+    bucket[fk.node.primaryFormIdentifiers.form_id] = {
+      ...bucket[fk.node.primaryFormIdentifiers.form_id],
+      formPrimaryIdentifierKeys: {
+        ...bucket[fk.node.primaryFormIdentifiers.form_id].formPrimaryIdentifierKeys,
+        ...getKeysValuePair([fk.node.name], feildState),
+      },
+    };
+  });
 
   // return an array of all the referenced forms 
   return !Object.keys(bucket).length ? [] : Object.keys(bucket).map(reference => (bucket[reference]))
@@ -84,8 +195,8 @@ export const ParseFormToGraphQL = (form, fields) => {
             { "connect": [ 
                             {"where": 
                               {"node": { "OR" : [
-                                              ...sortForeignKeys(form.ids, fields.formReferenceKeys),
-                                              { "primary_keys" : getKeysValuePair(fields.globalIdentifierKeys.map(id => id.name), form.ids)}
+                                              ...sortSubmitterByFormId(form.ids, fields.formReferenceKeys),
+                                              { "formPrimaryIdentifierKeys" : getKeysValuePair(fields.globalIdentifierKeys.map(id => id.name), form.ids)}
                                           ]
                                        }
                               } 
@@ -116,14 +227,77 @@ export const ParseFormToGraphQL = (form, fields) => {
   return {"fields" : {"create" : arr} }
 }
 
-export const parseFormFieldsToQueryContext = (fields,state) => {
-  if (fields === null || state === {}) return {}
-  return { "where" :
-           {"formPrimaryIdentifierKeys" :  fields.globalIdentifierKeys.length === 0 ? null :  getKeysValuePair(fields.globalIdentifierKeys.map(id => id.name), state) },
-           "referencePrimary": {
-           ...!fields.formReferenceKeys.length ? {} : {"OR": [
-              ...sortForeignKeys(state, fields.formReferenceKeys)
-            ]},
-            }
+export const parseFormFieldsToQueryContext = (fields, state) => {
+  if (fields === null || state === {}) return {};
+  return {
+    where: {
+      formPrimaryIdentifierKeys:
+        fields.globalIdentifierKeys.length === 0
+          ? null
+          : getKeysValuePair(
+              fields.globalIdentifierKeys.map((id) => id.name),
+              state
+            ),
+    },
+    referencePrimary: {
+      ...(!fields.formReferenceKeys.length
+        ? {}
+        : { OR: [...sortSubmitterByFormId(state, fields.formReferenceKeys)] }),
+    },
+  };
+};
+
+export const submitterBundleQueryParse = (
+  self,
+  root,
+  reference,
+  state,
+  form_id,
+  isRoot=false,
+) => {
+
+  console.log(isRoot)
+  if (isRoot){
+    return {
+      self : {
+        form: form_id,
+        formPrimaryIdentifierKeys:
+          self.length === 0
+            ? null
+            : getKeysValuePair(
+                self.map((id) => id.name),
+                state
+              ),
+      }
+    }
   }
-}
+
+
+  return {
+    root: {
+      formPrimaryIdentifierKeys:
+        root.length === 0
+          ? null
+          : getKeysValuePair(
+              root.map((id) => id.name),
+              state
+            ),
+    },
+    self: {
+      form: form_id,
+      formPrimaryIdentifierKeys:
+        self.length === 0
+          ? null
+          : getKeysValuePair(
+              self.map((id) => id.name),
+              state
+            ),
+    },
+    refrences: {
+      ...(!reference.length
+        ? {}
+        : { OR: [...sortSubmitterByFormId(state, reference)] }),
+    },
+    form: { form: form_id },
+  };
+};
