6bad289b937f54c46d5a6127c6fc217121d1b2d6
suluxan <suluxan.m@gmail.com>
2023-04-20 15:43:35 -0400

    updated welcome message but currently unused as Donor is set to default active item for formgenerator's metadata prop

--- ui/src/components/layout/WelcomeMessage.tsx
+++ ui/src/components/layout/WelcomeMessage.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { Image, Message } from 'semantic-ui-react'
+import { Image, Message, Segment } from 'semantic-ui-react'
 import logo from '../logos/logo.png'
 
 export const WelcomeMessage = () => {
     return (
-        <Message color={'teal'}>
+        <Segment color={'teal'}>
+        <Message >
             <Message.Header>Welcome to mCODER2!</Message.Header>
-            <p>Type a donor's ID in the search form or select a form on the left menu to start.</p>
-            <Image src={logo} size={'medium'} centered />
+            <p>Enter a Donor ID in the search form above or select a form on the left menu to begin abstracting.</p>
         </Message>
+
+        <Image src={logo} size={'large'} centered />
+
+        </Segment>
     )
 }
